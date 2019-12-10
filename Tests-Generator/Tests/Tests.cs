3b1f32f972c637012892e65c3d90af4c7058ea51
Dima Konovko <dima.konovko@mail.ru>
2019-12-10 21:08:05 +0300

    development; add: TestsGenerator and Tests projects

---
+++ Tests-Generator/Tests/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TestsGenerator;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        // variables
+        private string generatedClassesFolder;
+
+
+
+        [TestInitialize]
+        public async Task TestsInitialize()
+        {
+            // get all test classes
+            string testClassesFolder = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\TestClasses");
+            var allTestClasses = new List<string>();
+            foreach (string path in Directory.GetFiles(testClassesFolder, "*.cs"))
+            {
+                allTestClasses.Add(path);
+            }
+
+            // create folder for generated classes
+            generatedClassesFolder = System.IO.Path.Combine(testClassesFolder, "NUnitGeneretadClasses");
+            Directory.CreateDirectory(generatedClassesFolder);
+
+            // generate classes
+            NUnitTestsGenerator generator = new NUnitTestsGenerator(3, 3, 3);
+            await generator.GenerateAsync();
+        }
+
+
+
+        [TestMethod]
+        public void When_OneClass_Should_Generate_One_File()
+        {
+            
+        }
+    }
+}
---
+++ Tests-Generator/TestsGenerator/NUnitTestsGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TestsGenerator
+{
+    public class NUnitTestsGenerator
+    {
+        // variables
+        private ExecutionDataflowBlockOptions boMaxFilesToLoadCount;
+        private ExecutionDataflowBlockOptions boMaxExecuteTasksCount;
+        private ExecutionDataflowBlockOptions boMaxFilesToWriteCount;
+
+
+
+        // methods
+        public NUnitTestsGenerator(int maxFilesToLoadCount, int maxExecuteTasksCount, int maxFilesToWriteCount)
+        {
+            boMaxFilesToLoadCount  = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxFilesToLoadCount };
+            boMaxExecuteTasksCount = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxExecuteTasksCount };
+            boMaxFilesToWriteCount = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxFilesToWriteCount };
+        }
+
+
+
+        public void Generate(List<string> sourceFiles, string destFolder)
+        {
+            var loadFiles = new TransformBlock<string, FileInfo>(new Func<string, Task<FileInfo>>(LoadContent), boMaxFilesToLoadCount);
+        }
+
+
+
+        private async Task<FileInfo> LoadContent(string sourceFile)
+        {
+            string content;
+            using (var reader = new StreamReader(new FileStream(sourceFile, FileMode.Open)))
+            {
+                content = await reader.ReadToEndAsync(); 
+            }
+            return new FileInfo(sourceFile, content);
+        }
+    }
+}
