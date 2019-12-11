3c2b62450671a39088b9d14f2f4664212a71dbbb
Dima Konovko <dima.konovko@mail.ru>
2019-12-11 16:28:32 +0300

    development; add: UseExample project, upd: .gitignore

---
+++ Tests-Generator/UseExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsGenerator;
+
+namespace UseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Perform();
+        }
+
+
+        public void Perform()
+        {
+            Console.WriteLine("Enter maxFilesToLoadCount: ");
+            var maxFilesToLoadCount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter maxExecuteTasksCount: ");
+            var maxExecuteTasksCount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter maxFilesToWriteCount: ");
+            var maxFilesToWriteCount = int.Parse(Console.ReadLine());
+
+            string sourceFolder = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\Tests\\TestClasses");
+            string destFolder = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\Tests\\TestClasses\\GeneratedInRuntime");
+            Directory.CreateDirectory(destFolder);
+
+            new NUnitTestsGenerator(GetSourceFiles(sourceFolder), destFolder, maxFilesToLoadCount, maxExecuteTasksCount, maxFilesToWriteCount).Generate();
+
+            Console.WriteLine("Success");
+            Console.ReadLine();
+        }
+
+
+
+        private List<string> GetSourceFiles(string sourceFolder)
+        {
+            var sourceFiles = new List<string>();
+            foreach (string path in Directory.GetFiles(sourceFolder, "*.cs"))
+            {
+                sourceFiles.Add(path);
+            }
+
+            return sourceFiles;
+        }
+    }
+}
